58c59aee90ca70767261fb4166b45c6485a45752
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-07-06 20:40:31 +0600

    problem2 added

--- problem1.c
+++ problem1.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int n=100;
-    int i=0;
-    int result=0;
+    int n = 100;
+    int i = 0;
+    int result = 0;
 
-    for(i=1; i<n; i++)
+    for (i = 1; i < n; i++)
     {
-        if(i%3==0 || i%5==0)
+        if (i % 3 == 0 || i % 5 == 0)
             result += i;
     }
-    printf("%d\t", result);
+    printf("%d", result);
     printf("\n");
     return 0;
 }
